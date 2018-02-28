04ab312476f2691f58b5d0ab7be4ccd525d3e66b
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-02-28 13:56:43 +0100

    Modify Files

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
+$(function(){	
+	var carousel = $('#carousel'),
+		carouselList = $("#carousel ul"),
+		interval;
 
-$(function(){
+	var ANIMATION_TIMEOUT = 2000,
+		IMAGE_WIDTH = 400,
+		ANIMATION_DURATION = 500,
+		LEFT_DIRECTION = 'left',
+		RIGHT_DIRECTION = 'right';
 	
-	var carouselList = $("#carousel ul");
+	interval = setInterval(function() {
+		changeSlide(RIGHT_DIRECTION);
+	}, ANIMATION_TIMEOUT);
 	
-	setInterval(changeSlide, 3000);
-	
-	function changeSlide () {
-
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-		
-		function moveFirstSlide () {
-			var firstItem = carouselList.find("li:first");
-			var lastItem = carouselList.find("li:last");
-			lastItem.after(firstItem);
-			carouselList.css({marginLeft:0});
-		}
-		
+	function changeSlide(direction) {
+		switch (direction) {
+			case LEFT_DIRECTION:
+				moveLastSlide();
+				carouselList.animate({'marginLeft': 0}, ANIMATION_DURATION);
+				break;
+			case RIGHT_DIRECTION:
+				carouselList.animate({'marginLeft': -IMAGE_WIDTH}, ANIMATION_DURATION, moveFirstSlide);
+				break;
+		}						
 	}
-	
+
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: 0});
+	}	
+
+	function moveLastSlide () {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: -IMAGE_WIDTH});
+	}	
+
+	carousel.hover(
+		function() {
+			// wyłączam interval
+			clearInterval(interval);
+		},
+		function() {
+			// włączam interval
+			interval = setInterval(function() {
+				changeSlide(RIGHT_DIRECTION);
+			}, ANIMATION_TIMEOUT);
+		}
+	);
+
+	$('#next').click(function(e) {
+		e.preventDefault();
+		changeSlide(RIGHT_DIRECTION);
+	});	
+
+	$('#prev').click(function(e) {
+		e.preventDefault();
+		changeSlide(LEFT_DIRECTION);
+	});	
 });
